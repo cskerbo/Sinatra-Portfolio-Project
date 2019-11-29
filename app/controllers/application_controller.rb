247d938f3109dbdcd6d0e9728c61f76d0135fd36
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-29 21:53:26 +0000

    Correct controllers error due to file mixup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ require './config/environment'
 require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
