5ab1130e3ac5d7468c2ebbda2051a1d3c8dd8ae6
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-30 08:24:58 +0000

    Fixed scraper issues, now character_type is correct

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,51 +6,47 @@ class Scraper
   def self.scrape_survivors
     page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com/Survivors"))
     survivors = []
-
     survivor_list = page.css('div[style*="flex:1"]')
     survivor_list.each do |survivor|
-      character_type = "survivor"
       name = survivor.css("a").attribute("title").text
       bio_link = survivor.css('a').attribute('href').value
       bio_page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com#{bio_link}"))
       bio = bio_page.css('div.mw-parser-output i').each do |line|
         text = line.text.strip
         if text.length > 300
-          survivor_info = {:name => name, :bio => text, :character_type => character_type}
+          survivor_info = {:name => name, :bio => text}
           survivors << survivor_info
         end
       end
     end
-    #survivor_hash = survivors.group_by {|h1| h1[:name]}.map do |k, v|
-    #  {:name => k, :bio => v.map {|h2| h2[:bio] }.join}
-    #end
-  survivors
-  binding.pry
+    survivor_hash = survivors.group_by {|h1| h1[:name]}.map do |k, v|
+      {:name => k, :bio => v.map {|h2| h2[:bio] }.join}
+    end
+    survivor_hash.each {|t| t[:character_type]="survivor"}
+    survivor_hash
 end
-scrape_survivors
   def self.scrape_killers
     page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com/Dead_by_Daylight_Wiki"))
     killers = []
 
     killer_list = page.css('div#fpkiller.fpbox div.fplinks div.link')
     killer_list.each do |killer|
-      character_type = "killer"
       name = killer.css('a').text
       bio_link = killer.css('a').attribute('href').value
       bio_page = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com#{bio_link}"))
       bio = bio_page.css('div.mw-parser-output i').each do |line|
         text = line.text.strip
         if text.length > 300
-          killer_info = {:name => name, :bio => text, :character_type => character_type}
+          killer_info = {:name => name, :bio => text}
           killers << killer_info
         end
       end
     end
-    #killer_hash = killers.group_by {|h1| h1[:name]}.map do |k, v|
-    #  {:name => k, :bio => v.map {|h2| h2[:bio] }.join}
-
-  #  end
-    killers
+    killer_hash = killers.group_by {|h1| h1[:name]}.map do |k, v|
+      {:name => k, :bio => v.map {|h2| h2[:bio] }.join}
+    end
+    killer_hash.each {|t| t[:character_type]="killer"}
+    killer_hash
   end
 
   def self.scrape_perks
