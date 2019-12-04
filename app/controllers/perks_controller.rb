fa1ff14290f2e129476cb19100d6ead385f93b47
Chris Kerbo <chris.kerbo@outlook.com>
2019-12-04 18:06:13 +0000

    perk index edits

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -4,7 +4,7 @@ class PerksController < ApplicationController
  get '/perks' do
    @perks = Perks.all
    @image_list = Dir.glob("public/images/perks/*.{png}")
-   binding.pry
+
    erb :'perks/index'
  end
 end
