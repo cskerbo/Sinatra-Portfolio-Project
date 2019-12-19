7f6dc0a8251c3b6d5910249484a97c4d13f6c022
cskerbo <54558554+cskerbo@users.noreply.github.com>
2019-12-18 17:08:08 -0800

    adding user authentication to builds

--- app/controllers/build_controller.rb
+++ app/controllers/build_controller.rb
@@ -3,46 +3,64 @@ require 'sinatra/flash'
 class BuildController < ApplicationController
 
   get '/build' do
+    if logged_in?
       @characters = Character.all
       @perks = Perk.all
       @image_list = Dir.glob("public/images/perks/*.{png}")
       erb :'build/index'
+    else
+      redirect to '/login'
+      end
   end
 
   post '/build/new' do
-      @build = Build.create(params[:build])
+    if logged_in?
+      @build = current_user.builds.build(params[:build])
       @build.save
+      binding.pry
       redirect "build/new/#{@build.id}"
-
+    else
+      redirect to '/login'
+    end
   end
 
   get '/build/new/:id' do
-    @build = Build.find(params[:id])
-    @characters = Character.all
-    @perks = Perk.all
-    @image_list = Dir.glob("public/images/perks/*.{png}")
-    erb :'build/new'
+    if logged_in?
+      @build = Build.find(params[:id])
+      @characters = Character.all
+      @perks = Perk.all
+      @image_list = Dir.glob("public/images/perks/*.{png}")
+      erb :'build/new'
+    else
+      redirect to '/login'
+    end
   end
 
   patch '/build/:id' do
-    @build = Build.find(params[:id])
+    if logged_in?
+      @build = Build.find(params[:id])
       @build.update(params[:build])
       @build.save
       redirect to "/build/#{@build.id}"
+    else
+      redirect to '/login'
+    end
   end
 
 
   get '/build/:id' do
-    @perks = []
-    @image_list = Dir.glob("public/images/perks/*.{png}")
-    @build = Build.find(params[:id])
-    @build.perk_ids.each do |p|
+    if logged_in?
+      @perks = []
+      @image_list = Dir.glob("public/images/perks/*.{png}")
+      @build = Build.find(params[:id])
+      @build.perk_ids.each do |p|
       perk = Perk.find(p)
       @perks << perk
       end
-    erb :'build/show'
+      erb :'build/show'
+    else
+      redirect to '/login'
+    end
   end
-
-
 
 end
